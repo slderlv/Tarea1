a2a3ff2d87a206ff2afbb1dc9b09443859591c3f
claudeus123 <claudio.pizarro@alumnos.ucn.cl>
2022-09-09 20:53:00 -0300

    fixed bug in return

--- Calculate.java
+++ Calculate.java
@@ -1,5 +1,5 @@
 public class Calculate{
-    static void add(String number1, String number2){
+    static double add(String number1, String number2){
         return Double.parseDouble(number1) + Double.parseDouble(number2);
     }
 }
