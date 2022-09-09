4d14c2a6feaf07a3a55f71c051c6b034cd5ece9f
matias <mattreven@hotmail.com>
2022-09-09 18:11:34 -0400

    metodo suma

---
+++ suma.java
@@ -0,0 +1,9 @@
+class Suma {
+
+  public void suma(String num1,String num2){
+    double n1 = Double.parseDouble(num1);
+    double n2 = Double.parseDouble(num2);
+    int total = n1+n2;
+    System.out.print(total);
+  }
+}
