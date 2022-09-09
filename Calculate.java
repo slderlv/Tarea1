1e86a672385b2497aa1a806a1520a3e3c6287327
matias <mattreven@hotmail.com>
2022-09-09 18:14:56 -0400

    rest implemented

--- Calculate.java
+++ Calculate.java
@@ -1,5 +1,10 @@
 public class Calculate{
-    static double add(String number1, String number2){
+    static double sum(String number1, String number2){
         return Double.parseDouble(number1) + Double.parseDouble(number2);
     }
+	 
+    static double rest(String number1, String number2){
+        return Double.parseDouble(number1) - Double.parseDouble(number2);
+    }
+    
 }
--- suma.java
+++
@@ -1,9 +0,0 @@
-class Suma {
-
-  public void suma(String num1,String num2){
-    double n1 = Double.parseDouble(num1);
-    double n2 = Double.parseDouble(num2);
-    int total = n1+n2;
-    System.out.print(total);
-  }
-}
